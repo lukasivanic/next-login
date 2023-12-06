46b0cc9d88f442c53a47e1c360b6bc9acdfdbec3
lukasivanic <lukasivanic@icloud.com>
2023-12-06 23:50:23 +0100

    inputhandle

--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -9,34 +9,34 @@ import Label from "@/components/label";
 function Register() {
   const [inputs, setInputs] = useState({});
 
-  const handleChange = (event) => {
-    const name = event.target.name;
-    const value = event.target.value;
+  const inputHandle = (name, value) => {
     setInputs((values) => ({ ...values, [name]: value }));
   };
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    console.log("Form submitted");
     console.log(inputs);
   };
-
   return (
     <div id="id">
-      <form className="w-[450px] h-[600px] font-inter p-9 mt-12 mx-auto rounded-3xl shadow-[0_0_20px_1px_#bebebe]">
+      <form
+        onSubmit={handleSubmit}
+        className="w-[450px] h-[600px] font-inter p-9 mt-12 mx-auto rounded-3xl shadow-[0_0_20px_1px_#bebebe]"
+      >
         <h1 className="text-[2.6rem] font-semibold">Get started</h1>
         <h2 className="text-base ml-1 mb-10 mt-2 text-stone-400 font-normal">
           Create your account now
         </h2>
         <Label labelValue="User name" />
-        <Input name="name" type="text" onChange={handleChange} />
+        <Input name="name" type="text" inputHandle={inputHandle} />
 
         <Label labelValue="Email" />
-        <Input name="email" type="email" onChange={handleChange} />
+        <Input name="email" type="email" inputHandle={inputHandle} />
 
         <Label labelValue="Password" />
-        <Input type="password" name="password" onChange={handleChange} />
+        <Input type="password" name="password" inputHandle={inputHandle} />
 
-        <SubmitButton onSubmit={handleSubmit} value="Sign up" />
+        <SubmitButton value="Sign up" />
         <div className="text-sm text-stone-400 text-center">
           Already have account?{" "}
           <Link className="text-cyan-700 underline" href="/login">
--- src/components/input.jsx
+++ src/components/input.jsx
@@ -1,8 +1,14 @@
 export default function Input(props) {
+  function handleChange(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    props.inputHandle(name, value);
+  }
   return (
     <input
       type={props.type}
       name={props.name}
+      onChange={handleChange}
       className="block w-full rounded-xl my-3 border-0 py-2 px-3 text-black ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-gray-400 text-lg leading-7"
     />
   );
